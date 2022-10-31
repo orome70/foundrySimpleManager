b027cbf5ce1e4015f6a03a63c9734adc64b37e44
Christophe Morvan <christophe.morvan@jamespot.com>
2022-10-31 11:01:47 +0100

    Base i18n encore un bug désactivation.

--- scripts/simplescript.js
+++ scripts/simplescript.js
@@ -1,11 +1,13 @@
 function updateTabs(value) {
+    //const tabsBis = game.appliction
     const theTabs = ['[data-tab=scenes]','[data-tab=combat]','[data-tab=tables]','[data-tab=cards]','[data-tab=playlists]','[data-tab=compendium]'];
     
         theTabs.forEach(element => {
             if (value){
                 $(element).hide();
             } else {
-                $(element).show();
+                //$(element).activate();
+                game.activateTab(element);
             }
     });
 }
@@ -16,8 +18,9 @@ function updateTabs(value) {
 
 Hooks.on("init", function() {
     // Setting creation
+
     game.settings.register("SimpleManager", "minimal", {
-        name: "Simple interface",
+        name: game.i18n.localize("SMPLMGR.settingsText"),
         scope: "client",
         config: true,
         type: Boolean,
@@ -32,23 +35,26 @@ Hooks.on("init", function() {
    
 Hooks.on("ready", function() {
     game.settings.set('SimpleManager','minimal', true);
-    console.log("Minimal value ?");
-    console.log(game.settings.get('SimpleManager','minimal'));
-
+    
     // Choice dialog
     game.settings.set('SimpleManager','minimal', true);
+    const modalTitle = game.i18n.localize("SMPLMGR.modalTitle");
+    const modalText = game.i18n.localize("SMPLMGR.modalText");
+    const modalY = game.i18n.localize("SMPLMGR.modalChoiceY");
+    const modalN = game.i18n.localize("SMPLMGR.modalChoiceN");
+
     let d = new Dialog({
-        title: "Simplified interface",
-        content: "<p>You must choose if you wish to use a simplified interface</p>",
+        title: modalTitle,
+        content: "<p>"+modalText+"</p>",
         buttons: {
          one: {
           icon: '<i class="fas fa-check"></i>',
-          label: "Simplified interface",
+          label: modalY,
           callback: () => console.log("Simplified")
          },
          two: {
           icon: '<i class="fas fa-times"></i>',
-          label: "Classical interface",
+          label: modalN,
           callback: () => {
             console.log("Classical");
             game.settings.set('SimpleManager','minimal', false);
@@ -58,27 +64,11 @@ Hooks.on("ready", function() {
         default: "two",
         render: html => console.log("Register interactivity in the rendered dialog"),
         close: html => console.log("This always is logged no matter which option is chosen")
-       });
-       d.render(true);
+    });
+    d.render(true);
     
     document.getElementById("board").style["display"] = "none";
     console.log("This code runs once core initialization is ready and game data is available.");
     console.log("minimal value :");
     console.log(game.settings.get('SimpleManager','minimal'));
-    /*
-    if (game.settings.get('SimpleManager','minimal')){
-        $('[data-tab=scenes]').hide();
-        $('[data-tab=combat]').hide();
-        $('[data-tab=tables]').hide();
-        $('[data-tab=cards]').hide();
-        $('[data-tab=playlists]').hide();
-        $('[data-tab=compendium]').hide();
-        // Liste des autres tabs :
-        //$('[data-tab=chat]').hide();
-        //$('[data-tab=items]').hide();
-        //$('[data-tab=journal]').hide();
-        //$('[data-tab=actors]').hide();
-        //$('[data-tab=settings]').hide();    
-    }
-    */
   });
